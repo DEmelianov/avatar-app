e3a797227dd6ead1d37fdd8b75135bcf1e79c0c2
Daemoon-72 <daemoon72@gmail.com>
2024-04-04 02:11:03 +0500

    Change avatar style

--- src/js/main.js
+++ src/js/main.js
@@ -21,7 +21,7 @@ let IceServerUsername
 let IceServerCredential
 
 const TalkingAvatarCharacter = "lisa"
-const TalkingAvatarStyle = "casual-sitting"
+const TalkingAvatarStyle = "graceful-standing"
 
 supported_languages = ["en-US", "de-DE", "zh-CN", "ar-AE"] // The language detection engine supports a maximum of 4 languages
 
